7b7128d3b413c781e0c2bd8cf338612e084b6bbe
Matt <gordon-matt@hotmail.com>
2023-09-17 16:06:41 +0930

    Simple Code Generator: Structure for loaded JSON data is now displayed for easy reference

---
+++ Plugins/BBQ.Toolkit.Plugins.SimpleCodeGenerator/JsonHelper.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace BBQ.Toolkit.Plugins.SimpleCodeGenerator;
+
+public class JsonHelper
+{
+    public static IEnumerable<string> GetStructure(string json)
+    {
+        var result = new List<string>();
+        var token = JToken.Parse(json);
+        ProcessToken(result, token, string.Empty);
+        return result;
+    }
+
+    private static void ProcessToken(ICollection<string> result, JToken token, string path)
+    {
+        switch (token.Type)
+        {
+            case JTokenType.Object:
+                foreach (var prop in token.Children<JProperty>())
+                {
+                    ProcessToken(result, prop.Value, Prefix(path, prop.Name));
+                }
+                break;
+
+            case JTokenType.Array:
+                int index = 0;
+                foreach (var childToken in token.Children())
+                {
+                    ProcessToken(result, childToken, Prefix(path, "[]"));
+                    index++;
+                }
+                break;
+
+            default:
+                if (!result.Contains(path))
+                {
+                    result.Add(path);
+                }
+                break;
+        }
+    }
+
+    private static string Prefix(string path, string name) => string.IsNullOrEmpty(path) ? name : $"{path}.{name}";
+}
